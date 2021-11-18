df9bcf4d9532b17a4f088ba1848717d24b101cec
TonyHan <ourclub7279@gmail.com>
2021-11-19 01:14:41 +0900

    norm

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -16,25 +16,25 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int		ft_printChar(int);
-int		ft_formats(va_list , const char);
-int		ft_printf(const char *, ...);
+int	ft_printChar(int c);
+int	ft_formats(va_list args, const char format);
+int	ft_printf(const char *str, ...);
 
-void	ft_putStr(char *);
-int		ft_printStr(char *);
-int		ft_printNbr(int);
-int		ft_printPercent(void);
+void	ft_putStr(char *str);
+int	ft_printStr(char *str);
+int	ft_printNbr(int num);
+int	ft_printPercent(void);
 
-int		ft_unsigned_len(unsigned int);
-char	*uitoa(unsigned int);
-int		ft_printf_unsigned(unsigned int);
+int	ft_unsigned_len(unsigned int n);
+char	*uitoa(unsigned int n);
+int	ft_printf_unsigned(unsigned int n);
 
-int		ft_ptr_len(unsigned long long);
-void	ft_putPtr(unsigned long long);
-int		ft_printPtr(unsigned long long);
+int	ft_ptr_len(unsigned long long ptr);
+void	ft_putPtr(unsigned long long ptr);
+int	ft_printPtr(unsigned long long ptr);
 
-int		ft_hex_len(unsigned int);
-void	ft_putHex(unsigned int, const char);
-int		ft_printHex(unsigned int, const char);
+int	ft_hex_len(unsigned int n);
+void	ft_putHex(unsigned int n, const char format);
+int	ft_printHex(unsigned int n, const char format);
 
 #endif
